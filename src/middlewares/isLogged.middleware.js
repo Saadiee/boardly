07543767fae554941146061a9a964e245e5a3e4b
Saad <saad_eew@hotmail.com>
2025-04-16 23:53:25 +0500

    feat: update isLoggedIn middleware to use refresh token for authentication

--- src/middlewares/isLogged.middleware.js
+++ src/middlewares/isLogged.middleware.js
@@ -2,12 +2,13 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { ApiError } from "../utils/api-error.js";
 import { asyncHandler } from "../utils/async-handler.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const isLoggedIn = asyncHandler(async (req, res, next) => {
-  const jwtToken = req.cookies?.jwtToken;
-  if (!jwtToken) {
+  const getRefreshToken = req.cookies?.refreshToken;
+  if (!getRefreshToken) {
     return res
       .status(401)
       .json(
@@ -17,8 +18,16 @@ export const isLoggedIn = asyncHandler(async (req, res, next) => {
         ),
       );
   }
-  const decodedJwt = jwt.verify(jwtToken, process.env.JWT_SECRET);
-  req.user = decodedJwt;
-
-  next();
+  try {
+    const decodedToken = jwt.verify(
+      getRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+    );
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json(new ApiError(401, "Invalid or expired refresh token"));
+  }
 });
