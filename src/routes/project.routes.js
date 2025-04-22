b8a83aff4f073925735064c1cc643c3428033fb7
Saad <saad_eew@hotmail.com>
2025-04-22 23:53:06 +0500

    feat: ensure authentication middleware is applied to project update, delete, and member routes

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -17,9 +17,10 @@ const router = Router();
 router.route("/create-project").post(isLoggedIn, createProject);
 router.route("/get-project-by-id").post(isLoggedIn, getProjectById);
 router.route("/get-projects").get(isLoggedIn, getProjects);
-router.route("/update-project").post(updateProject);
-router.route("/deleteProject").delete(deleteProject);
-router.route("/project-members/:projectId").get(getProjectMembers);
+router.route("/update-project").post(isLoggedIn, updateProject);
+router.route("/deleteProject").delete(isLoggedIn, deleteProject);
+router.route("/project-members/:projectId").get(isLoggedIn, getProjectMembers);
 router.route("/add-members/:projectId").post(isLoggedIn, addMemberToProject);
+router.route("/delete-member").post(isLoggedIn, deleteMember);
 
 export default router;
